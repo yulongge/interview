b9b86e42902ddb254912ef1734367361d7af1f1e
gaiyulong <gyl@acewill.cn>
2020-07-28 19:21:25 +0800

    inserTionSort

--- sort.js
+++ sort.js
@@ -1,6 +1,7 @@
 /**
  * 排序
  * 参考：https://segmentfault.com/a/1190000016286789
+ * 参考：https://www.runoob.com/w3cnote/selection-sort.html
  */
 var sortArr = [7,2, 5, 4, 6, 8, 9];
 
@@ -25,12 +26,69 @@ function bubbleSort(arr) {
     console.log(arr, 'after sort')
 }
 
+function bubbleSortDown(arr) {
+	var len = arr.length;
+	for(var i=0; i<len; i++) {
+		for(var j =0; j < len - i -1; j++) {
+			var temp = arr[j];
+			if(arr[j] < arr[j + 1]) {
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	console.log(arr, 'after bubbleSortDown')
+}
+
+//bubbleSort(sortArr);
+//bubbleSortDown(sortArr);
+
 /**
  * 选择排序
- * 选择排序也很简单。它每一次从待排序的数据元素中选出最小（或最大）的一个元素，存放在序列的起始位置，直到全部待排序的数据元素排完
- * 选择排序的时间复杂度为O(n2)，空间复杂度为O(1)，属于 稳定 排序。适用于数据比较少的情况，综合各种情况来讲还是这个最慢。
+ * 选择排序是一种简单直观的排序算法，无论什么数据进去都是 O(n²) 的时间复杂度。所以用到它的时候，数据规模越小越好。唯一的好处可能就是不占用额外的内存空间了吧
+ * 
+ * 1.首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置。
+ * 2.再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+ * 3.重复第二步，直到所有元素均排序完毕
  */
 
+ function selectionSort(arr) {
+	var len = arr.length;
+	for(var i = 0; i< len - 1; i++) {
+		var minIndex = i; 
+		var minItem = arr[i];
+		for(var j = i + 1; j<len; j++) {
+			if(arr[minIndex] > arr[j]) {
+				minIndex = j;
+			}
+		}
+		arr[i] = arr[minIndex]; 
+		arr[minIndex] = minItem;
+	}
+ }
 
-bubbleSort(sortArr);
+ //selectionSort(sortArr)
+
+ /**
+  * 插入排序
+  * 第一待排序序列第一个元素看做一个有序序列，把第二个元素到最后一个元素当成是未排序序列。
+  * 从头到尾依次扫描未排序序列，将扫描到的每个元素插入有序序列的适当位置。（如果待插入的元素与有序序列中的某个元素相等，则将待插入元素插入到相等元素的后面。）
+  */
+ function insertionSort(arr) {
+	var len = arr.length;
+	for(var i=0;i<len;i++) {
+		var temp = arr[i];
+		var inserIndex = i -1;
+		for(var j=inserIndex;j>0;j--) {
+			if(arr[j]>temp) {
+				arr[j+1] = arr[j]; 
+				inserIndex --;
+				//arr[j] = temp;
+			}
+		}
+		arr[inserIndex] = temp;
+	}
+ }
+
+ insertionSort(sortArr)
 console.log(sortArr, 'sortArr')
